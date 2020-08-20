985d3af85ff0d9fc136fe2602c61e7279c124bb5
bwasti <bwasti@fb.com>
2020-08-20 17:16:41 +0000

    Make repo compatible with pytorch/hub

--- mobilenetv3.py
+++ mobilenetv3.py
@@ -233,18 +233,13 @@ def mobilenetv3(pretrained=False, **kwargs):
 
 if __name__ == '__main__':
     net = mobilenetv3()
+    net = torch.jit.script(net)
     print('mobilenetv3:\n', net)
     print('Total params: %.2fM' % (sum(p.numel() for p in net.parameters())/1000000.0))
     input_size=(1, 3, 224, 224)
-    # pip install --upgrade git+https://github.com/kuan-wang/pytorch-OpCounter.git
-    from thop import profile
-    flops, params = profile(net, input_size=input_size)
-    # print(flops)
-    # print(params)
-    print('Total params: %.2fM' % (params/1000000.0))
-    print('Total flops: %.2fM' % (flops/1000000.0))
     x = torch.randn(input_size)
     out = net(x)
+    print(out.shape)
 
 
 
